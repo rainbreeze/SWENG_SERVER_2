b8fadf2c4b7c43572157daafe8a9c75d8e1190e1
rainbreeze <rainbreeze2@naver.com>
2024-11-17 17:52:51 +0900

    변수이름 연결

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // server.js
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors'); // CORS 모듈 추가
+const cors = require('cors');
 const db = require('./database');  // DB 연결 객체 임포트
-const routes = require('./routes')
+const userRouter = require('./user_routers');  // routes에서 내보낸 userRouter 사용
 
 const app = express();
 const port = 3000;
@@ -15,8 +15,8 @@ dotenv.config();
 app.use(express.json());  // 요청 본문을 JSON으로 파싱
 app.use(cors());  // CORS 설정
 
-// 라우팅 설정
-app.use('/', routes);  // 모든 라우트를 '/' 경로로 처리
+// 라우터 설정
+app.use('/', userRouter);  // '/' 경로로 모든 라우트를 처리
 
 // 서버 실행
 app.listen(port, () => {
