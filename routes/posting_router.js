36824f21add449dd0e11adb30abf2f2c4a5f985a
joji <rainbreeze2@naver.com>
2024-11-20 11:47:39 +0900

    posting router생성자에 db객체 생성하는거 삭제

--- routes/posting_router.js
+++ routes/posting_router.js
@@ -3,26 +3,27 @@ const express = require('express');
 const PostingController = require('../controllers/posting_controller'); // 게시글 컨트롤러 임포트
 
 class PostingRouter {
+    #router;
+
     constructor(db) {
-        this.db = db; 
-        this._router = express.Router();
+        this.#router = express.Router();
         this.postingController = new PostingController(db); // DB 객체를 주입하여 PostingController 생성
         this.initializeRoutes();
     }
 
     // 라우트 초기화
     initializeRoutes() {
-        this._router.post('/', (req, res) => this.postingController.createPosting(req, res)); // 게시글 추가
-        this._router.get('/', (req, res) => this.postingController.getPostings(req, res)); // 모든 게시글 조회
-        this._router.get('/:id', (req, res) => this.postingController.getPostingById(req, res)); // 특정 게시글 조회
-        this._router.post('/:id/like', (req, res) => this.postingController.likePosting(req, res)); // 게시글 좋아요
-        this._router.post('/:id/comment', (req, res) => this.postingController.addCommentToPosting(req, res)); // 게시글 댓글
-        this._router.delete('/:id', (req, res) => this.postingController.deletePosting(req, res));  // 게시글 삭제
+        this.#router.post('/', (req, res) => this.postingController.createPosting(req, res)); // 게시글 추가
+        this.#router.get('/', (req, res) => this.postingController.getPostings(req, res)); // 모든 게시글 조회
+        this.#router.get('/:id', (req, res) => this.postingController.getPostingById(req, res)); // 특정 게시글 조회
+        this.#router.post('/:id/like', (req, res) => this.postingController.likePosting(req, res)); // 게시글 좋아요
+        this.#router.post('/:id/comment', (req, res) => this.postingController.addCommentToPosting(req, res)); // 게시글 댓글
+        this.#router.delete('/:id', (req, res) => this.postingController.deletePosting(req, res));  // 게시글 삭제
     }
 
     // 라우터 반환
     getRouter() {
-        return this._router;
+        return this.#router;
     }
 }
 
